f642069f16d972e7aa4b28d7c8f31b8cfece337e
Ivan <ivan.radovic@acme.co.me>
2023-04-10 08:42:12 +0200

    AddingButtonForFavorites

--- screens/MealDetailsScreen.jsx
+++ screens/MealDetailsScreen.jsx
@@ -1,14 +1,32 @@
-import { View,Text, Image, StyleSheet, ScrollView } from "react-native";
+import { useLayoutEffect } from "react";
+import { View,Text, Image, StyleSheet, ScrollView, Button } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetails/Subtitle";
 import List from "../components/MealDetails/List";
+import IconButton from "../components/IconButton";
 
-const MealDetailsScreen = ({ route }) => {
+const MealDetailsScreen = ({ route, navigation }) => {
     const mealId = route.params.mealId;
     
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+
+    /* -- Making meal favorite --  */
+    const headerButtonPressHandler = () => {
+      console.log("Pressed")
+    }
+
+
+    /* --- Set IconButton to the header ---  */
+    useLayoutEffect(() => {
+      navigation.setOptions({
+        headerRight: () => {
+            return <IconButton icon='star' color='white' onPress={headerButtonPressHandler}/>
+        }});
+    }, [navigation, headerButtonPressHandler])
+
 
     return ( 
         <ScrollView style={styles.rootContainer}>
